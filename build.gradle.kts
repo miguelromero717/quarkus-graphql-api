443d91d436cf60bcf7a8c5bc97a83670d3b6c2be
Miguel Romero <miguelromero717@gmail.com>
2023-02-28 14:50:51 +0100

    Set up API with DB

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    implementation("io.quarkus:quarkus-jdbc-postgresql")
+    implementation("io.quarkus:quarkus-hibernate-orm-panache-kotlin")
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-resteasy-jackson")
     implementation("io.quarkus:quarkus-kotlin")
